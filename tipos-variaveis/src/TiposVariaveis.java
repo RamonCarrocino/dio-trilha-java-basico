7d06be4a43a33d4d6d8ec1a49a3af187ce49e792
Ramon Carrocino <ramon.carrocino@gmail.com>
2024-06-09 22:54:33 -0300

    Save

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        final int numero = 5;
+      
+    System.out.print (numero);
+
+        final double valor_de_pi = 3.14;
+       
+    System.out.print (valor_de_pi);
     }
 }
