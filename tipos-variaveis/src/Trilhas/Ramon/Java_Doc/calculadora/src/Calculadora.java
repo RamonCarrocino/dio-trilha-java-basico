c11c358d126ddad10ecb414ff607c3184088d5b6
Ramon Carrocino <ramon.carrocino@gmail.com>
2024-06-14 23:36:36 -0300

    Linhas de comentários

---
+++ tipos-variaveis/src/Trilhas/Ramon/Java_Doc/calculadora/src/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Nota:</b> Leia atentamente a documentação dessa classe
+ * para desfrutar dos recursosoferecidos pelo autor
+ * 
+ * @author Ramon Carrocino
+ * @version 1.0
+ * @since 14/06/2024
+ */
+public class Calculadora {
+    /**
+     * Esse método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o prímeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @retur int o resultado deste método é a soma dos dois números
+     */
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+    }
+
---
+++ tipos-variaveis/src/Trilhas/Ramon/Java_Doc/calculadora/src/Comentarios.java
@@ -0,0 +1,32 @@
+public class Comentarios {
+    public static void main(String[] args) {
+            // Olá, eu sou um comentário em uma única linha
+    
+            /* Olá,
+             * Eu sou um comentário
+             * que posso ser mais detalhado
+             * quando necessário
+             */
+    
+        }
+        /**
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+        */
+
+        /*
+        * javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../doc src/*.java
+        * Esse comando é para criar um "manual" do programa.
+        * Ele é executado direto no terminal.
+        * (-encoding UTF-8), é para manter a acentuação, quando necessário.
+        * (-d ../doc), Volta um nível de pasta e cria o arquivo doc.
+        * (src/*.java), Vou fazer um manual de todos os arquivos do tipo java,
+        * que estão na pasta src.
+        */
+        public void metodo (){
+
+        }
+}
