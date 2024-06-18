8f83c0d75a793098c74415b1961f993b6e02f7a2
Ramon Carrocino <ramon.carrocino@gmail.com>
2024-06-18 20:27:29 -0300

    Estruturas Excepcionais

---
+++ Estruturas Excepcionais/src/AboutMe.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {    
+    public static void main(String[] args) {
+        try{
+        //Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println ("Digite seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println ("Digite seu Sobrenome:");
+        String sobreNome = scanner.next();
+
+        System.out.println ("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println ("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
+        //Imprimindo os dados obtidos pelo usuário
+        System.out.println("Meu nome é " + nome + " " + sobreNome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "m ");
+        scanner.close();
+        }
+        catch (InputMismatchException e) {
+            System.out.println ("Idade de altura devem ser numéricos.");
+        }
+    }
+}
